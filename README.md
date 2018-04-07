c8783378b32ba4f21c132ae1c89b03af0870cc1a
vcordero07 <vcordero07@gmail.com>
2018-04-07 16:31:38 -0400

    wip-functional login et signup page

---
+++ README.md
@@ -0,0 +1,1 @@
+## Node Password Reset
--- app.js
+++ app.js
@@ -1,28 +1,194 @@
-var express = require("express");
-var path = require("path");
-// var favicon = require("static-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const async = require("async");
+const crypto = require("crypto");
 
-var app = express();
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: "Incorrect username." });
+      user.comparePassword(password, function(err, isMatch) {
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password." });
+        }
+      });
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+});
+
+UserSchema.pre("save", function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+var User = mongoose.model("User", UserSchema);
+
+mongoose.Promise = global.Promise;
+const { PORT, DATABASE_URL } = require("./config");
+const app = express();
 
 // Middleware
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-// app.use(favicon());
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "memento-mori",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.get("/", function(req, res) {
-  res.render("index", { title: "Express" });
+  res.render("index", {
+    title: "Express",
+    user: req.user
+  });
+});
+
+app.get("/login", function(req, res) {
+  res.render("login", {
+    user: req.user
+  });
+});
+
+app.get("/signup", function(req, res) {
+  res.render("signup", {
+    user: req.user
+  });
+});
+
+app.post("/login", function(req, res, next) {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) return next(err);
+    if (!user) {
+      return res.redirect("/login");
+    }
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      return res.redirect("/");
+    });
+  })(req, res, next);
+});
+
+app.post("/signup", function(req, res) {
+  var user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  user.save(function(err) {
+    req.logIn(user, function(err) {
+      res.redirect("/");
+    });
+  });
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(app.get("port"), function() {
   console.log("Express server listening on port " + app.get("port"));
 });
+
+let server;
+
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(PORT, () => {
+          //console.log(`Your app is listening on port ${PORT}`);
+          resolve();
+        })
+        .on("error", err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      //console.log(`Closing server`);
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+module.exports = { app, runServer, closeServer };
